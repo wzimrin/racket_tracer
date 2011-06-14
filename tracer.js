844fd61ea87bf78b043da55683c59975e159d212
Jeanette Miranda <jmiranda@cs.brown.edu>
2011-06-14 16:14:44 -0400

    commented

--- tracer.js
+++ tracer.js
@@ -1,32 +1,20 @@
-$('.shrunkenCall').live('click',function (event) {
-  var target = $(this)
-  target.data("oldClass",target.attr("class"))
-  target.data("oldHTML",target.contents())
-
-  target.data('node').expanded = true
-  showTree(target.data('node'),target)
-})
-
-$('.delButton').live('click',function (event) {
-  var target = $(event.target)
-  var parent = target.parents('.expandedCall').first()
-  parent.data('node').expanded = false
-  parent.empty()
-  parent.attr("class",parent.data("oldClass"))
-  parent.append(parent.data("oldHTML"))
-  event.stopPropagation();
-})
+//William Zimrin and Jeanette Miranda
+//tracer.js	6/02/2011
 
 function element(tag) {
   return $("<"+tag+'/>')
 }
 
+//Creates a shrunken call -- the function, actuals and results in their correct
+//expanded or collapsed form. Will alternate the colors of the background to 
+//layer child calls on top of parent calls
 function makeShrunkenCall(child,parent) {
   var newDisplay = element('div');
   newDisplay.addClass('shrunkenCall');
   newDisplay.addClass('call');
   $(newDisplay).data("node",child)
 
+  //Layer background colors so that child is opposite of its parent
   if (parent.hasClass("background1"))
     newDisplay.addClass('background2');
   else
@@ -35,31 +23,44 @@ function makeShrunkenCall(child,parent) {
   table = element('table')
   var row = element('tr');
 
-  funcName = element('td'); funcName.addClass('name');
-  funcName.text(child.name); row.append(funcName);
+  //Function Name
+  funcName = element('td'); 
+  funcName.addClass('name');
+  funcName.text(child.name); 
+  row.append(funcName);
 
-  for (j = 0; j < child.actuals.length; j++) {
+  //Actuals (choose expanded or unexpanded form based on boolean stored in node)
+  for (j = 0; j < child.actuals.length; j++) 
+  {
     var arg = element('td');
     if (child.actualsExpanded == undefined || child.actualsExpanded[j] == false || child.actualsExpanded[j] == undefined)
    	 arg.text(child.actualsShort[j]);
     else if(child.actualsExpanded[j] == true) 
 	    arg.text(child.actuals[j])
     arg.data("type", ["actualsExpanded", j])
-    row.append(arg)}
+    row.append(arg)
+  }
 
-  arrow = element('td'); arrow.addClass('arrow'); 
-  arrow.text(' => '); row.append(arrow);
+  //Arrow
+  arrow = element('td')
+  arrow.addClass('arrow') 
+  arrow.text(' => ')
+  row.append(arrow)
 
-  result = element('td');
-  result.text(child.resultShort);
-  row.append(result);
+  //Resort -- shortened form of result for shrunkenCalls
+  result = element('td')
+  result.text(child.resultShort)
+  row.append(result)
 
-  table.append(row);
-  newDisplay.append(table);
+  table.append(row)
+  newDisplay.append(table)
   
   return newDisplay
 }
 
+//Helper to make and argument -- used for both formals and actuals
+//Will check to see if two forms are the same, and if they are, will not make
+//expandable/collapsible
 function makeArg(arg,otherForm, type, node) {
   var TD = element('td')
   TD.addClass("arg")
@@ -80,12 +81,13 @@ function showTree(traceNode, displayWhere) {
   displayWhere = $(displayWhere)
   displayWhere.empty()
   
-  var upperTable = element('table');
-  displayWhere.append(upperTable);
+  var upperTable = element('table')
+  displayWhere.append(upperTable)
 
-  var upperTR = element('tr');
-  actualsTR = element('tr');
+  var upperTR = element('tr')
+  actualsTR = element('tr')
 
+  //Delete button
   var delTD = element('td')
   delTD.attr("rowspan",2)
   delTD.addClass('delButton');
@@ -94,12 +96,15 @@ function showTree(traceNode, displayWhere) {
   delButton.attr("rowspan",2);
   delTD.append(delButton);
   upperTR.append(delTD)
+
+  //Function name
   var nameTD = element('td')
   nameTD.attr("rowspan",2)
   nameTD.text(traceNode.name)
   nameTD.addClass("name")
   upperTR.append(nameTD)
 
+  //Formals and actuals
   for (var i = 0; i < traceNode.formals.length; i++) {
     if(traceNode.formalsExpanded == undefined 
 		    || traceNode.formalsExpanded[i] == undefined 
@@ -116,12 +121,14 @@ function showTree(traceNode, displayWhere) {
 	actualsTR.append(makeArg(traceNode.actuals[i], traceNode.actualsShort[i], ["actualsExpanded", i], traceNode))
        	  }
 
+  //Arrow
   var arrow = element('td')
   arrow.attr("rowspan",2)
   arrow.text("=>")
   arrow.addClass("arrow")
   upperTR.append(arrow)
 
+  //Result
   var resultTD = element('td')
   resultTD.attr("rowspan",2)
   resultTD.addClass("result")
@@ -191,6 +198,35 @@ $(document).ready(function () {
   first.trigger('click')
 })
 
+// ----------------------------------------------------------------------------
+//                                      EVENTS
+// ----------------------------------------------------------------------------
+
+
+//EVENT: Expands shrunkenCall (child) on click
+//Stores current class and html (know what to restore to if an expanded child 
+//is collapsed) and then expands the child
+$('.shrunkenCall').live('click',function (event) {
+  var target = $(this)
+  target.data("oldClass",target.attr("class"))
+  target.data("oldHTML",target.contents())
+  target.data('node').expanded = true
+  showTree(target.data('node'),target)
+})
+
+//EVENT: Collapses the expandedCall (child) on click
+//Stores the current class and html (know what to restore to if collapsedCall 
+//is expanded again) and then collapses the child
+$('.delButton').live('click',function (event) {
+  var target = $(event.target)
+  var parent = target.parents('.expandedCall').first()
+  parent.data('node').expanded = false
+  parent.empty()
+  parent.attr("class",parent.data("oldClass"))
+  parent.append(parent.data("oldHTML"))
+  event.stopPropagation();
+})
+
 $('ul.tabs li.other').live('click', function (event) {
   target = $(this)
   var div = $("#tracer")
@@ -205,6 +241,11 @@ $('ul.tabs li.other').live('click', function (event) {
   target.removeClass("other")
 })
 
+
+//EVENT: Expandable/collapsible object on click
+//When an expandable or collapsible object is clicked, swap to its other form
+//And update the appropriate expanded boolean denoting whether it is expanded
+//or collapse
 $(".expandable").live("click", function (event) {
   target = $(this)
   var newText = target.data("otherForm")
@@ -227,12 +268,3 @@ $(".expandable").live("click", function (event) {
   target.text(newText)
 })
 
-/*
-function find(object,lst) {
-  var obj = object
-  for (var i = 0; i < lst.length; i++) {
-    obj = obj[lst[i]]
-  }
-  return obj
-}
-*/
