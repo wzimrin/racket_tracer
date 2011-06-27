6aa212ac2f8ba4be54392439c46e714fd6521207
Jeanette Miranda <jmiranda@cs.brown.edu>
2011-06-27 16:28:25 -0400

    examples directory with saved traces and racket code

---
+++ examples/2011_06_27/brown-heaps.rkt
@@ -0,0 +1,74 @@
+#lang planet tracer/tracer
+
+(define-struct bhnode (value left right))
+
+(define (insert x h)
+  (cond
+    [(empty? h) (make-bhnode x empty empty)]
+    [(bhnode? h)
+     (let [(y (bhnode-value h))
+           (l (bhnode-left h))
+           (r (bhnode-right h))]
+       ;always insert into the right (smaller) subtree
+       (if (< x y)
+         ;once done, make sure that is the left (larger) subtree
+         (make-bhnode x (insert y r) l)
+         ;we are inserting into an empty heap
+         (make-bhnode y (insert x r) l)))]))
+
+(define (make-heap ns)
+  (foldl insert empty ns))
+ 
+(define get-min bhnode-value)
+
+(define (remove-min h)
+  
+  (local [;makes a new tree from x and the l and r subtrees
+          (define (insert-merge x l r)
+            ;easy, since no structure changes - simply move values around
+            (cond [(andmap 
+                    (lambda (y)
+                      ;if x is less than the get-min of all existing subtrees
+                      (or (not (bhnode? y))
+                          (< x (get-min y))))
+                    (list l r))
+                   ;we can simply put x here - it is a valid tree
+                   (make-bhnode x l r)]
+                  [;if r is empty or if l is less than r
+                   (or (not (bhnode? r))
+                       (< (get-min l) (get-min r)))
+                   ;insert it into l and use l as the new min of the tree
+                   (make-bhnode (get-min l)
+                                (insert-merge x (bhnode-left l) (bhnode-right l));we don't insert it into 
+                                r)];r if r is empty because we don't want to change the structure
+                  [(or (not (bhnode? l));proceed as above, with l and r flipped
+                       (< (get-min r) (get-min l)))
+                   (make-bhnode (get-min r)
+           
+                                l
+                                (insert-merge x (bhnode-left r) (bhnode-right r)))]))
+          (define (left-most h);gets the left-most value (the only one we can easily remove)
+            (let ([l (bhnode-left h)]);go left until l is empty, then return the value
+              (cond [(bhnode? l) (left-most l)]
+                    [(empty? l) (bhnode-value h)])))
+          (define (remove-left-most h);removes the left-most value
+            ;we can, since the left subtree is always >= the right tree
+            ;we just move the left subtree to the right afterwards
+            (let ([l (bhnode-left h)])
+              (cond [(bhnode? l);go left until l is empty, than rebuild the tree without the node above the empty l
+                     (make-bhnode (bhnode-value h); we can do this, because if l is empty, r has to be empty for it to
+                                  (bhnode-right h);be a BrownHeap
+                                  (remove-left-most l))]
+                    [(empty? l) empty])))
+          (define extra (left-most h));the new value that we will put into the root and then fix
+          (define new-h (remove-left-most h))];the heap without extra
+    (cond [(empty? new-h) empty];we removed the last value
+          [(bhnode? new-h)
+           (insert-merge extra;new-h is a valid BrownHeap, so this results in a valid BrownHeap
+                  (bhnode-left new-h)
+                  (bhnode-right new-h))])))
+
+(define heap (make-heap (list 8 4 3 9 1 6 12 14)))
+(check-expect (remove-min heap) (make-heap (list 8 4 3 9 6 12 14)))
+(check-expect (get-min heap) 1)
+(check-expect (map (lambda (x) (add1 x)) (list 1 2 3)) (list 1 2 3))
---
+++ examples/2011_06_27/other.rkt
@@ -0,0 +1,80 @@
+#lang planet tracer/tracer
+
+(define (fib x)
+  (if (< x 2)
+      x
+      (+ (fib (- x 1))
+         (fib (- x 2)))))
+
+(define (add x y)
+  (+ x y))
+
+(define (squared x)
+  (* x x))
+
+(define (dist x1 y1 x2 y2)
+  (sqrt (add (squared (- x1 x2))
+             (squared (- y1 y2)))))
+
+(define (close-enough? x y)
+  (< (dist x y 0 0) 4))
+
+(define (all-close-enough? xs)
+  (andmap (lambda (x)
+            (close-enough? (first x) (second x)))
+          xs))
+
+(fib 10)
+
+(close-enough? 3 3)
+
+(all-close-enough? '((1 1) (2 2) (3 3) (4 4)))
+
+(check-expect (fib 5) (+ (fib 4) (fib 2)))
+(check-expect (fib 5) (+ (fib 4) (fib 3)))
+(check-expect (fib 5) 1)
+
+(define-struct dir (name dirs files))
+(define-struct file (name size content))
+
+;Capital letters are folders, lower case are files
+;Folder A has folders B, C, D and files e, f, g in it
+;A
+;B              C          D  e f g
+;H   I j k      l m        n
+;o   
+
+(define e1 (make-file "e" 1 empty))
+(define f (make-file "f" 1 empty))
+(define g (make-file "g" 1 empty))
+(define j (make-file "j" 1 empty))
+(define k (make-file "k" 1 empty))
+(define l (make-file "l" 1 empty))
+(define m (make-file "m" 1 empty))
+(define n (make-file "n" 1 empty))
+(define o (make-file "o" 1 empty))
+(define H (make-dir "H" empty (list o)))
+(define I (make-dir "I" empty empty))
+(define B (make-dir "B" (list H I) (list j k)))
+(define C (make-dir "C" empty (list l m)))
+(define D (make-dir "D" empty (list n)))
+(define A (make-dir "A" (list B C D) (list e1 f g)))
+
+;Consumes: a directory
+;Produces: number of files in this directory and subdirectories
+(: how-many (dir -> Number))
+(define (how-many d)
+  (cond
+    ;No more directories - number of files in this directory
+    [(empty? (dir-dirs d)) (length (dir-files d))] 
+    ;More directories - number of files in this directory and in subdirectories 
+    [(cons? (dir-dirs d))
+     (+ (length (dir-files d))
+        (how-many (first (dir-dirs d)))
+        (how-many (make-dir
+                   (dir-name d)
+                   (rest (dir-dirs d))
+                   empty)))]))
+
+;9
+(how-many A)
