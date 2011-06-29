d8f0c5db9f384d9c2628c58deefec641ab29f327
Jeanette Miranda <jmiranda@cs.brown.edu>
2011-06-29 13:08:13 -0400

    webpage titled with file name

--- tracer/lang/reader.rkt
+++ tracer/lang/reader.rkt
@@ -8,14 +8,19 @@
     (port-count-lines! in)
     (define reloc-in (relocate-input-port in 1 1 offset))
     (port-count-lines! reloc-in)
-    (let* ([mod (rd reloc-in)]
-           [mod  (if stx? mod (datum->syntax #f mod))]
-           [r (with-syntax ([port-text port-text]
-                            [offset offset])
-                (syntax-case mod ()
-                  [(module name lang* (modbegin . body))
-                   (syntax/loc mod
-                     (module name lang* (modbegin port-text offset . body)))]))])
+    (let*-values ([(mod) (rd reloc-in)]
+                  [(mod)  (if stx? mod (datum->syntax #f mod))]
+                  [(path fileName c) (split-path (syntax-source mod))]
+                  [(r) (with-syntax ([port-text port-text]
+                                     [offset offset]
+                                     [fileName fileName])
+                         (syntax-case mod ()
+                           [(module name lang* (modbegin . body))
+                            (syntax/loc mod
+                                ;added name after modbeg
+                                (module name lang* (modbegin (path->string fileName)
+                                                             port-text 
+                                                             offset . body)))]))])
       (if stx? r (syntax->datum r)))))
 
 (require racket/port)
