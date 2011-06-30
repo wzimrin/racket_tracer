1aa0a70a41ff204bd032ac0494c9c8f1ca8b5a5f
William Zimrin <retief1@gmail.com>
2011-06-30 13:04:24 -0400

    two demos

---
+++ demos/check-expect-demo.rkt
@@ -0,0 +1,31 @@
+#lang planet tracer/tracer
+
+#|
+Shows how check expects are displayed.  Check expects that pass are uninteresting,
+so are not displayed.  Check expects that fail, however, are displayed.  In this case,
+we also see the usefulness of the tracer.  The bug in these functions is in dist, but
+due to the poor choice of tests, the bug doesn't show up until the test for 
+close-enough?.  However, when we inspect what happened in the incorrect call to
+close-enough?, we can easily see that the error (or at least an error) lies in dist.
+|#
+
+(check-expect (square 2) 4)
+(check-expect (square 3) 9)
+(define (square x)
+  (* x x))
+
+(check-expect (dist 4 5 1 5) 3)
+(check-expect (dist 10 2 3 2) 7)
+(define (dist x1 y1 x2 y2)
+  (sqrt (+ (square (- x1 x2))
+           (- y1 y2))))
+
+(check-expect (close-enough? '(2 2)) #t)
+(check-expect (close-enough? '(2 3)) #f)
+(define (close-enough? pt)
+  (< (dist (first pt) (second pt) 0 0) 3))
+
+(check-expect (find-close-enough? '((1 2) (2 3) (2 2)))
+             '((1 2) (2 2))) 
+(define (find-close-enough? lst)
+  (filter close-enough? lst))
---
+++ demos/image-demo.rkt
@@ -0,0 +1,70 @@
+#lang planet tracer/tracer
+
+(require 2htdp/image)
+
+(check-expect (sq 2) 4)
+(define (sq x)
+  (* x x))
+
+(check-expect (add-point (list 1 2) (list 3 4))
+              (list 4 6))
+(define (add-point p1 p2)
+  (map + p1 p2))
+
+(check-expect (scale-point 2 (list 1 2))
+              (list 2 4))
+(define (scale-point x p)
+  (map (lambda (i) (* x i))
+       p))
+
+(check-expect (sub-point (list 3 4) (list 1 2))
+              (list 2 2))
+(define (sub-point p1 p2)
+  (map - p1 p2))
+
+(check-expect (dist (list 1 2) (list 4 6)) 5)
+(define (dist p1 p2)
+  (sqrt (apply +
+               (map sq (sub-point p2 p1)))))
+
+(check-expect (weighted-avg 4 7) 5)
+(define (weighted-avg x y)
+  (/ (+ x x y) 3))
+
+(check-expect (mid-point (list 2 3) (list 4 5))
+              (list 3 4))
+(define (mid-point p1 p2)
+  (scale-point 1/2 (add-point p1 p2)))
+
+(check-expect (third-point (list 1 2) (list 4 5)) (list 2 3))
+(define (third-point p1 p2)
+  (map weighted-avg p1 p2))
+
+(define (make-fractal depth side)
+  (local [(define (draw-fractal canvas depth p1 p2)
+            (let* ([mid1 (third-point p1 p2)]
+                   [mid2 (third-point p2 p1)]
+                   [middle (mid-point p1 p2)]
+                   [side (/ (dist p1 p2) 3)]
+                   [angle (+ (/ pi 2)
+                             (apply atan (reverse (sub-point p2 p1))))]
+                   [point (add-point middle
+                                     (scale-point side
+                                                  (list (cos angle)
+                                                        (sin angle))))]
+                   [new-depth (sub1 depth)])
+              (if (<= depth 0)
+                  (add-line canvas
+                            (first p1) (second p1)
+                            (first p2) (second p2)
+                            "red")
+                  (draw-fractal (draw-fractal (draw-fractal (draw-fractal canvas 
+                                                                          new-depth p1 mid1)
+                                                            new-depth mid2 p2)
+                                              new-depth mid1 point)
+                                new-depth point mid2))))]
+    (draw-fractal (rectangle side (/ side 2) "solid" "white")
+                  depth
+                  (list 0 0) (list side 0))))
+
+(make-fractal 3 80)
