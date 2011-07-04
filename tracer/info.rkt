2b1cc8af0f82f9a045b4b62fa155c4e12cf5c423
William Zimrin <retief1@gmail.com>
2011-07-03 20:12:28 -0400

    added info.rkt

---
+++ tracer/info.rkt
@@ -0,0 +1,9 @@
+#lang setup/infotab
+(define name "Tracer")
+(define blurb '("Traces the execution of a functional program. To use set lang to #lang planet tracer/tracer, do not use require."))
+(define repositories '("4.x"))
+(define required-core-version "5.1.1")
+(define primary-file "tracer.rkt")
+(define categories '(devtools))
+(define scribblings '(("docs/tracer.scrbl")))
+(define release-notes '((p "Minor bugfixes")))
