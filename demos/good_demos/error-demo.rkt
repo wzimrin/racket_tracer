b86bc82cb940cb958962b7ae636d5140598231ad
William Zimrin <retief1@gmail.com>
2011-08-04 13:20:06 -0400

    check-*s work

--- demos/good_demos/error-demo.rkt
+++ demos/good_demos/error-demo.rkt
@@ -7,4 +7,18 @@
     [#t (+ (fib (- x 1))
            (fib (- x 2)))]))
 
-(fib 3)
+;(check-expect (fib 2) 1)
+
+(check-within (fib 1) 0 1)
+(check-within (fib 1) 0 0)
+
+(check-error (fib 1))
+
+(check-expect (fib 1) 2)
+
+(check-member-of 1 1 2 3 4)
+(check-member-of 5 1 2 3 4)
+
+(check-range 3 2 5)
+(check-range 2 3 5)
+(check-range 5 2 3)
